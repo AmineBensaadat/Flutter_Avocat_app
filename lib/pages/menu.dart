e3011ba7e63b6f25ff643e1aae45e16bf6084ba9
bensaadat <bensaadat.amine@gmail.com>
2023-05-08 13:58:06 +0100

    add shared button icon

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 
 import '../shared/appbar.dart';
+import '../shared/menu_icon_button.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -16,96 +17,125 @@ class _MenuState extends State<Menu> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Appbar(),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 15,
+      body: Column(children: [
+        SizedBox(
+          height: 15,
+        ),
+        Center(
+          child: Text(
+            "Menu",
+            style: TextStyle(
+                fontSize: 40,
+                color: Color(0xFF101751),
+                fontWeight: FontWeight.bold),
           ),
-          Center(
-            child: Text(
-              "Menu",
-              style: TextStyle(
-                  fontSize: 40,
-                  color: Color(0xFF101751),
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          SingleChildScrollView(
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 40,
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 20, 10, 15),
-                      child: IconButton(
-                        icon:
-                            Image.asset('assets/images/Cltsgriséexxxhdpi.png'),
-                        onPressed: () {},
-                      ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          child: Row(
+            children: [
+              SizedBox(
+                width: 40,
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 20, 10, 0),
+                    child: MenuIconButton(
+                        icon_img: 'assets/images/Cltsgriséexxxhdpi.png',
+                        screen_name: "screen to open"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Text(
+                      "Clients",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFFe7e7e7),
+                          fontWeight: FontWeight.bold),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(right: 10),
-                      child: Text(
-                        "Clients",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    )
-                  ],
-                ),
-                Spacer(),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 20, 10, 15),
-                      child: IconButton(
-                        icon: Image.asset(
-                            'assets/images/Contactsgriséxxxhdpi.png'),
-                        onPressed: () {},
-                      ),
+                  )
+                ],
+              ),
+              Spacer(),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 20, 10, 0),
+                    child: MenuIconButton(
+                        icon_img: 'assets/images/Contactsgriséxxxhdpi.png',
+                        screen_name: "screen to open"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Text(
+                      "Contacts",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFFe7e7e7),
+                          fontWeight: FontWeight.bold),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(right: 10),
-                      child: Text(
-                        "Contacts",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    )
-                  ],
-                ),
-                Spacer(),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 20, 10, 15),
-                      child: IconButton(
-                        icon: Image.asset(
-                            'assets/images/Dossiersgriséexxxhdpi.png'),
-                        onPressed: () {},
-                      ),
+                  )
+                ],
+              ),
+              Spacer(),
+              Column(
+                children: [
+                  Container(
+                      padding: EdgeInsets.fromLTRB(0, 20, 10, 0),
+                      child: MenuIconButton(
+                          icon_img: 'assets/images/Dossiersgriséexxxhdpi.png',
+                          screen_name: "screen to open")),
+                  Container(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Text(
+                      "Dossiers",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFFe7e7e7),
+                          fontWeight: FontWeight.bold),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(right: 10),
-                      child: Text(
-                        "Dossiers",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  width: 40,
-                ),
-              ],
-            ),
+                  )
+                ],
+              ),
+              SizedBox(
+                width: 40,
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Container(
+          child: Row(
+            children: [
+              SizedBox(
+                width: 40,
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 20, 10, 0),
+                    child: MenuIconButton(
+                        icon_img: 'assets/images/Agenda.png',
+                        screen_name: "screen to open"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Text(
+                      "Agenda",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFFe7e7e7),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color(0xFF101751),
         items: <BottomNavigationBarItem>[
---
+++ lib/shared/menu_icon_button.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class MenuIconButton extends StatelessWidget {
+  final String icon_img;
+  final String screen_name;
+
+  MenuIconButton({
+    Key? key,
+    required this.icon_img,
+    required this.screen_name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Image.asset(icon_img),
+      onPressed: () {
+        print(screen_name);
+      },
+    );
+  }
+}
